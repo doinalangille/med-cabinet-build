654c360940fdbad8e4cc4d06480d3e3572e7b680
Doina Langille <doina.jitoreanu@gmail.com>
2020-03-04 10:35:19 -0500

    test the model with strain id

--- web_app/app.py
+++ web_app/app.py
@@ -1,39 +1,58 @@
 from flask import Flask, json
+from json import JSONEncoder
 from flask_cors import CORS
 import pandas as pd
+import numpy as np
 from sklearn.externals import joblib
-from sklearn.neighbors import NearestNeighbors 
+from sklearn.neighbors import NearestNeighbors
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+
 
 def create_app():
     app = Flask(__name__)
-    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+    # cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+    data = pd.read_csv('C:/Users/doina/OneDrive/Desktop/Lambda-School/3_Data-Engineering/med-cabinet-build/strains_text.csv')
+    # model = joblib.load('C:/Users/doina/OneDrive/Desktop/Lambda-School/3_Data-Engineering/med-cabinet-build/strains_recomender4.joblib')
 
-    @app.route('/id/<int:post_id>')
-    def home(strain_id):
-        data = pd.read_csv('./web_app/strains_text.csv')
-        model = joblib.load('./web_app/strains_recomender4.joblib')
-        strain = data[data['id'] == strain_id]
-        strain = strain['Combined']
-        sample = strain.head()
-        #preds = model.kneighbors(strain)
-        return sample
-        
-        # TO DO implement pred
-        
-        # 
-        # df = songs100[(songs100['track_index_num'] == preds[1][0][0]) | (songs100['track_index_num'] == preds[1][0][1]) | (songs100['track_index_num'] == preds[1][0][2]) | (songs100['track_index_num'] == preds[1][0][3]) | (songs100['track_index_num'] == preds[1][0][4])]
-        # dict_set = [{
-        # 'track_index_num' : x[0],
-        # 'track_id' : x[1],
-        # 'track_name' : x[2],
-        # 'artist_name' : x[3],
-        # 'album_cover_url' : x[4]
-        # }
-        # for x in df[['track_index_num', 'track_id', 'track_name', 'artist_name', 'album_cover_url']].values]
+    # Model section
+    def predict():
+        tfidf = TfidfVectorizer(stop_words='english', max_features=5000)
+        dtm = tfidf.fit_transform(data['Combined'])
+        dtm = pd.DataFrame(dtm.todense(), columns=tfidf.get_feature_names())
+        nn = NearestNeighbors(n_neighbors=4, algorithm='kd_tree')
+        var = nn.fit(dtm)
+        return dtm, var
 
-        # json_preds = json.dumps(dict_set)
+    # test_string = """I would like to be happy, energetic, and to get rid of my headache. My favorite flavor is blueberry."""
 
-        # return json_preds
-        
+    @app.route('/')
+    def home():
+        return "This is the Home Page"
     
+    @app.route('/id/<int:strain_id>')
+    # THIS IS A TEST
+    def get_strains(strain_id):
+        strain_index = data[data['id']==strain_id].index.values.astype(int)[0]
+        results = predict()
+        dtm = results[0]
+        fitting = results[1]
+        pred = fitting.kneighbors([dtm.iloc[strain_index].values])
+        recomended = pred[1]
+        return json.dumps(recomended, cls=NumpyArrayEncoder)
+      
+    def decode(input_str):
+        return input_str.replace("%22", " ").replace("%20", " ").replace("%7B", " ").replace("%7D", " ")
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return 'This page does not exist', 404
+
     return app
--- web_app/test.py
+++ web_app/test.py
@@ -1,11 +1,24 @@
+from flask import Flask, jsonify
+# from flask_cors import CORS
 import pandas as pd
 from sklearn.externals import joblib
-from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import NearestNeighbors 
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
-def home(strain_id):
-    data = pd.read_csv('C:/Users/doina/OneDrive/Desktop/Lambda-School/3_Data-Engineering/med-cabinet-build/strains_text.csv')
-    model = joblib.load('C:/Users/doina/OneDrive/Desktop/Lambda-School/3_Data-Engineering/med-cabinet-build//strains_recomender4.joblib')
-    strain = data[data['id'] == strain_id]
-    strain = strain['Combined']
-    preds = model.kneighbors(strain)
-    return preds
+
+
+
+data = pd.read_csv('C:/Users/doina/OneDrive/Desktop/Lambda-School/3_Data-Engineering/med-cabinet-build/strains_text.csv')
+#model = joblib.load('C:/Users/doina/OneDrive/Desktop/Lambda-School/3_Data-Engineering/med-cabinet-build/tfidf.joblib')
+
+tfidf = TfidfVectorizer(stop_words='english', max_features=5000)
+dtm = tfidf.fit_transform(data['Combined'])
+dtm = pd.DataFrame(dtm.todense(), columns=tfidf.get_feature_names())
+nn = NearestNeighbors(n_neighbors=4, algorithm='kd_tree')
+var = nn.fit(dtm)
+
+print 
+
+
+
+
